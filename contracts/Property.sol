ae67910bf650ead137488e731f1a26307aa07a59
ming.yan <ming.yan@sumscope.com>
2022-06-26 15:52:43 +0800

    init project

---
+++ contracts/Property.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Property is ERC721, ERC721URIStorage, Ownable {
+    
+}
